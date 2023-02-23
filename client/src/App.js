393d71c2d087e4e432a2abe7c0c6509d557b5770
Melissa Vaiden <melissavaiden@gmail.com>
2023-02-23 17:59:56 -0500

    removed reportwebvitals and app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
-import './App.css';
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
-
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      Hello
     </div>
   );
 }
