483de86fc47a24d3b244781b04cfce65c9eed3ac
Melissa Vaiden <melissavaiden@gmail.com>
2023-02-24 12:00:49 -0500

    aligned buttons on home page

--- client/src/components/Banners-NavBar/LandingPageBanner.js
+++ client/src/components/Banners-NavBar/LandingPageBanner.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import LoginButton from '../Buttons/LoginButton'
+import SignUpButton from '../Buttons/SignUpButton'
 
 function LandingPageBanner() {
   return (
-    <div>Test your skills today!</div>
+    <div className='landing-page-banner'>
+      Test your skills today!
+    </div>
   )
 }
 
--- client/src/components/Pages/LandingPage.js
+++ client/src/components/Pages/LandingPage.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import LandingPageBanner from '../Banners-NavBar/LandingPageBanner'
+import LoginButton from '../Buttons/LoginButton'
+import SignUpButton from '../Buttons/SignUpButton'
 
 function LandingPage() {
   return (
     <div>
+        <div className='container'>
+            <div className='landing-page-signup-btn'>
+                <SignUpButton />
+            </div>
+             <div className='landing-page-login-btn'>
+                <LoginButton />
+            </div> 
+        </div>
         <LandingPageBanner />
+
     </div>
   )
 }
