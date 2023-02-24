47d4f89993780917219c4b48730529d90ac7041c
Melissa Vaiden <melissavaiden@gmail.com>
2023-02-24 10:03:50 -0500

    fixed issue with react-router-dom versions

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './index.css';
+import { Routes, Route } from 'react-router-dom';
 import PageTitle from "./components/PageTitle";
-import LoginButton from "./components/LoginButton";
+import LoginButton from "./components/Buttons/LoginButton";
 
 function App() {
 
   return (
     <div className="App">
-      <PageTitle />
-      <LoginButton />
+        <Routes>
+          {/* <Route path='/login'>
+            <LoginPage />
+          </Route> */}
+          <Route exact path='/' element={ <LoginButton /> }></Route>
+        </Routes>
     </div>
   );
 }
