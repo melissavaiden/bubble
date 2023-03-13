90fd878eef185fada8ae144f4fe1b935537c5b06
Melissa Vaiden <melissavaiden@gmail.com>
2023-03-13 19:36:13 -0400

    function to validate correct answer

--- client/src/components/Pages/GamePlayPage.js
+++ client/src/components/Pages/GamePlayPage.js
@@ -3,15 +3,35 @@ import NavBar from '../Banners-NavBar/NavBar'
 import InGameTimer from '../Timer/InGameTimer'
 
 function GamePlayPage({category, difficulty, allQuestions}) {
+  const [nextQuestionIndex, setNextQuestionIndex] = useState(1)
   const [currentQuestion, setCurrentQuestion] = useState(allQuestions[0])
-  const [nextQuestion, setNextQuestion] = useState(allQuestions[1])
- console.log(currentQuestion.answers)
+  const [nextQuestion, setNextQuestion] = useState(allQuestions[nextQuestionIndex])
+  const [score, setScore] = useState(0)
+
+  console.log(currentQuestion.correct_answer)
+  console.log(nextQuestionIndex)
+  console.log(score)
+
+  function handleClick(e) {
+      let selectedAnswer = e.target.innerHTML
+      let correctAnswer = currentQuestion.correct_answer
+      if (selectedAnswer === correctAnswer) {
+        setCurrentQuestion(nextQuestion)
+        setNextQuestionIndex(nextQuestionIndex + 1)
+        setScore(score + 1)
+        console.log('u win')
+      }
+      else {
+        nextQuestionIndex++
+        console.log('u lose')
+      }
+  }
 
 
   let gameQuestion = <div className='card'>{currentQuestion.question_text}</div>
 
   let gameAnswers = currentQuestion.answers.map((answer) => {
-    return <div key={answer} className='btn'>{answer}</div>
+    return <div key={answer} className='btn' onClick={handleClick} value={answer}>{answer}</div>
   })
 
 
